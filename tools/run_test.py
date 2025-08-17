617a57525f57d456996f2959c3a74af0a1fa6282
Halcik <julkatablet2507@gmail.com>
2025-08-17 11:48:13 +0200

    Adding run a student (example now) code in a new process with timeout

--- tools/run_test.py
+++ tools/run_test.py
@@ -23,29 +23,76 @@ def fake_input(prompt=""):
     return ""
 
 
-def test_script(example_data):
+def script_worker(student_code: str, q: multiprocessing.Queue):
+  '''Uruchamia skrypt w osobnym procesie'''
+  global input_data_list, input_index # bo fake_input bierze globalne
+  input_data_list = q.get()
+  input_index = 0
   output_test = StringIO()
   try:
     with redirect_stdout(output_test):
-      exec(example_data)
+      env = {"__name__":"__main__", "input": fake_input} #podmianka do przekazania
+      exec(student_code, env, env) # sorce, globals, locals
   except Exception as e:
-    output = f"Wystąpił błąd: {e}"
+    q.put(("err", f"Wystąpił błąd: {e}"))
   else:
-    output = output_test.getvalue().strip()
-  return output  
+    q.put(("ok", output_test.getvalue().strip())) 
 
-def test_function(example_file, function_name):
-  global input_data_list
+
+def function_worker(student_path_file: str, function_name: str, name_of_file: str, q: multiprocessing.Queue):
+  """Worker, który ładuje moduł w osobnym procesie i wywołuje funkcję."""
+  try:
+    input_data_list = q.get()
+    spec = importlib.util.spec_from_file_location(name_of_file, student_path_file)
+    modul = importlib.util.module_from_spec(spec)
+    sys.modules[name_of_file] = modul
+    spec.loader.exec_module(modul)
+
+    func = getattr(modul, function_name) # zwraca obiekt funkcji z modułu.
+    output = func(*input_data_list) # normalnie się wywołuje
+    q.put(("ok", output))
+  except Exception as e:
+    q.put(("err", f"Wystąpił błąd: {e}"))
+
+
+def test_script(example_data, timeout):
+  '''Funkcja do testowania skryptu'''
+  q = multiprocessing.Queue()
+  q.put(input_data_list)
+  p = multiprocessing.Process(target=script_worker, args=(example_data, q))
+  p.start()
+
+  p.join(timeout)
+  if p.is_alive():
+    p.terminate()
+    p.join() # dla upewnienia
+    return "TIMEOUT"
+  if q.empty():
+    return "BRAK WYNIKU"
+  
+  status, output = q.get()
+  return output
+
+
+def test_function(example_file, function_name, timeout):
+  '''Funkcja do testowania wyjść (return) funkcji'''
   path_to_import = str(example_file)
   name_of_file = example_file.stem
 
-  spec = importlib.util.spec_from_file_location(name_of_file, path_to_import)
-  modul = importlib.util.module_from_spec(spec)
-  sys.modules[name_of_file] = modul
-  spec.loader.exec_module(modul)
+  q = multiprocessing.Queue()
+  q.put(input_data_list)
+  p = multiprocessing.Process(target=function_worker, args=(path_to_import, function_name, name_of_file, q))
+  p.start()
 
-  func = getattr(modul, function_name) # zwraca obiekt funkcji z modułu.
-  output = func(*input_data_list) # normalnie się wywołuje
+  p.join(timeout)
+  if p.is_alive():
+    p.terminate()
+    p.join() # dla upewnienia
+    return "TIMEOUT"
+  if q.empty():
+    return "BRAK WYNIKU"
+  
+  status, output = q.get()
   return output
 
 
@@ -70,13 +117,10 @@ def print_test_result(output_data, output, checker) -> int:
   return 0
 
 
-def test_code(task_path, type):
+def test_code(task_path, type_test):
   "Funkcja służąca do uruchamiania testów na napisanym kodzie - obecnie na przykładach"
-  global input_index
-  global input_data_list
+  global input_index, input_data_list
 
-  original_input = input
-  globals()["input"] = fake_input
   passed = 0
   total = 0
 
@@ -92,9 +136,10 @@ def test_code(task_path, type):
     example_data = example.read()
 
   function_name = task_data['function_name']
+  timeout = task_data['time_limit']
 
-  print(colorama.Style.BRIGHT+colorama.Fore.BLUE+f"\n~~ {type.replace('_', ' ').title()} ~~ ")
-  for test in test_data[type]:
+  print(colorama.Style.BRIGHT+colorama.Fore.BLUE+f"\n~~ {type_test.replace('_', ' ').title()} ~~ ")
+  for test in test_data[type_test]:
     print(colorama.Style.RESET_ALL, end="")
     total+=1
     output_data = test['expected_output'].strip()
@@ -106,14 +151,13 @@ def test_code(task_path, type):
       input_data_list = []
 
     if function_name:
-      output = test_function(example_file, function_name)  
+      output = test_function(example_file, function_name, timeout)  
     else:
-      output = test_script(example_data)
+      output = test_script(example_data, timeout)
 
     passed += print_test_result(output_data, output, test['checker'])  
 
   print(colorama.Style.BRIGHT+colorama.Fore.BLUE+f"\n{passed}/{total} testów przeszło ({round(passed/total*100) if total else 100}%)")
-  globals()["input"] = original_input
 
 if __name__ == '__main__':
   task_path = Path(__file__).resolve().parent.parent / 'tasks'
